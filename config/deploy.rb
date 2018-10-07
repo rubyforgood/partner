1c14e651c80dcedb9d6f4200391376cbda9eb15c
Sean Marcia <smarcia@gmail.com>
2018-10-07 14:45:49 -0400

    Setting up new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.10.2"
+lock "3.11.0"
 
 set :repo_url,        "git@github.com:rubyforgood/partner.git"
 set :application,     "diaper_partner"
