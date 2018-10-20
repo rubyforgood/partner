cd25fbd94e303a5233d9da3a74b8f381b410d9c6
Sean Marcia <smarcia@gmail.com>
2018-10-20 13:41:18 -0400

    require api-key for partner creation.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get ":name", to: "static#page"
+  get "pages/:name", to: "static#page"
   root "static#index"
 end
