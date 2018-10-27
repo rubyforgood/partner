57b9c3ec3e6cab95014b5ae4cc43904dc686774a
Chase Southard <chase.southard@gmail.com>
2018-10-27 15:29:04 -0400

    adds a couple of partners approved and pending

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating partners..."
+puts "Adding an 'approved' partner."
+Partner.create(
+    executive_director_name: "Leslie Knope",
+    program_contact_name: "Leslie Knope",
+    name: "Pawnee Indiana Diaper Bank",
+    address1: "123 Main St",
+    address2: "",
+    city: "Pawnee",
+    state: "IN",
+    zip_code: 62558,
+    website: "http://pawneeindiana.com",
+    zips_served: 62558,
+    executive_director_email: "leslie@example.com",
+    email: "leslie@example.com",
+    password: "password",
+    partner_status: "approved"
+)
+puts "Adding a generic 'pending' partner."
+Partner.create(
+    email: "pending@example.com",
+    password: "password"
+)
+puts "Done creating partners."
