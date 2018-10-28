6434f518233e19750c5993e2bb75b01336bd5267
Chase Southard <chase.southard@gmail.com>
2018-10-28 11:03:11 -0400

    some validations on items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :partner_request, optional: true
+  validates_presence_of :name, :quantity
+  validates :quantity, numericality: {only_integer: true}
 end
