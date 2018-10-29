4ca6b15bba295274b3594bcf76a1d20cc8475902
Chase Southard <chase.southard@gmail.com>
2018-10-29 18:41:13 -0400

    adds tests for verified?

--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -10,4 +10,25 @@ describe Partner, type: :model do
 
     expect(partner).to be_valid
   end
+
+  context 'partner with a verfied status' do
+    it 'verfied? returns a partner verified status as true' do
+      partner = build(:partner, partner_status: 'Verified')
+      expect(partner.verified?).to be true
+    end
+  end
+
+  context 'partner with a pending status' do
+    it 'verfied? returns a partner verified status as false' do
+      partner = build(:partner)
+      expect(partner.verified?).to be false
+    end
+  end
+
+  context 'partner with an unknown status' do
+    it 'verfied? returns a partner verified status as false' do
+      partner = build(:partner, partner_status: '')
+      expect(partner.verified?).to be false
+    end
+  end
 end
