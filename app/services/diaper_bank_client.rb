aaf7244695970d9f0a7d233eaa44f32e2b9f3898
Chase Southard <chase.southard@gmail.com>
2018-10-28 21:24:10 -0400

    adds a request submission post
    first pass at this

--- app/services/diaper_bank_client.rb
+++ app/services/diaper_bank_client.rb
@@ -4,10 +4,25 @@ module DiaperBankClient
 
     partner = { partner:
       { diaper_partner_id: partner_id} }
-        
+
     uri = URI(ENV["DIAPERBANK_APPROVAL_URL"])
     req = Net::HTTP::Post.new(uri, "Content-Type" => "application/json")
     req.body = partner.to_json
+    req["Content-Type"] = "application/json"
+    req["X-Api-Key"] = ENV["DIAPERBANK_KEY"]
+
+    response = https(uri).request(req)
+
+    response.body
+  end
+
+  def self.request_submission_post(partner_request_id)
+    return if Rails.env != "production"
+    return unless PartnerRequest.exists?(partner_request_id)
+
+    uri = URI(ENV["DIAPERBANK_PARTNER_REQUEST_URL"])
+    req = Net::HTTP::Post.new(uri, "Content-Type" => "application/json")
+    req.body = PartnerRequestBlueprint.render(PartnerRequest.find(partner_request_id))
     req["Content-Type"] = "application/json"
     req["X-Api-Key"] = ENV["DIAPERBANK_KEY"]
 
