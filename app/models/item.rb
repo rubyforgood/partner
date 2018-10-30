4f08caf4f5f349b3ccbd24679567b55a8845d2d9
Chase Southard <chase.southard@gmail.com>
2018-10-29 21:12:38 -0400

    update validations for partner_request and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :partner_request, optional: true
   validates_presence_of :name, :quantity
-  validates :quantity, numericality: {only_integer: true}
+  validates_inclusion_of :name, in: POSSIBLE_ITEMS.keys
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
--- app/models/partner_request.rb
+++ app/models/partner_request.rb
@@ -1,5 +1,8 @@
 class PartnerRequest < ApplicationRecord
   belongs_to :partner, dependent: :destroy
+
   has_many :items, dependent: :destroy
   accepts_nested_attributes_for :items, allow_destroy: true, reject_if: proc { |attributes| attributes['quantity'].blank? }
+
+  validates_presence_of :partner
 end
