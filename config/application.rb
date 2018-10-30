d77266d782b0f5c05c73b090c3bf2038431ef6c6
Chase Southard <chase.southard@gmail.com>
2018-10-30 09:51:57 -0400

    ack! remove last remnants of blueprinter

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,5 @@ module Partnerbase
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.autoload_paths << Rails.root.join("blueprints")
-    config.eager_load_paths << Rails.root.join("blueprints")
   end
 end
