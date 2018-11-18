8a80dae22661c2728871d0af922e0d1d99dba52f
Chase Southard <chase.southard@gmail.com>
2018-11-17 22:14:13 -0500

    adds partner policy with CRUD actions

---
+++ app/policies/partner_policy.rb
@@ -0,0 +1,25 @@
+# Authorization for CRUD actions on Partner using Pundit
+#
+# Pundit relies on current_user
+#   see `pundit_user` method in partners_controller
+#
+# Partners can only affect their own records.
+# Later may want to admins authorization.
+class PartnerPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def show?
+    record == user
+  end
+
+  def update?
+    record == user
+  end
+
+  # TODO(chaserx): I think we should prevent deletion until we add a soft delete
+  def destroy?
+    false
+  end
+end
