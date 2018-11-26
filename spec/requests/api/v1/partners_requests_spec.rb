c830045bf851fa04a7907f677ea855c01eac71ba
David Curtis <dacur7@gmail.com>
2018-11-26 11:25:58 -0500

    Removes explicit calls to FactoryBot; remove unused spec.

--- spec/requests/api/v1/partners_requests_spec.rb
+++ spec/requests/api/v1/partners_requests_spec.rb
@@ -1,20 +1,6 @@
 require "rails_helper"
 
 describe "Partners Api Requests" do
-  #   describe 'GET "/api/v1/partners' do
-  #     it "returns all partners" do
-  #       partner = FactoryBot.create(:partner)
-
-  #       get api_v1_partners_path
-
-  #       expect(response).to have_http_status(:ok)
-
-  #       results = JSON.parse(response.body)
-  #       expect(results.size).to eq 1
-  #       expect(results[0]["diaper_partner_id"]).to eq(partner.id)
-  #     end
-  #   end
-
   describe "GET /api/v1/partners/1" do
     let(:partner) { create(:partner) }
 
