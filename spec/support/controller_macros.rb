2c36f56edc0d454ca45f94598f4ae546dceea16b
David Curtis <dacur7@gmail.com>
2018-12-02 15:54:23 -0500

    Makes @partner available in specs.

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,8 +2,8 @@ module ControllerMacros
   def login_partner
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:partner]
-      partner = create(:partner)
-      sign_in partner
+      @partner = create(:partner)
+      sign_in @partner
     end
   end
 end
