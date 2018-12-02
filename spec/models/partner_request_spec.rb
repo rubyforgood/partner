dd6d67ac165c5b62e09bb9358f343497809a0917
David Curtis <dacur7@gmail.com>
2018-12-02 13:33:20 -0500

    Adds partner and partner request specs; updates partner request model.

--- spec/models/partner_request_spec.rb
+++ spec/models/partner_request_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe PartnerRequest, type: :model do
+  it { is_expected.to have_many(:items).dependent(:destroy) }
+
+  it { is_expected.to validate_presence_of(:partner) }
+
   describe "a valid PartnerRequest" do
     it "requires a Partner" do
       expect(build_stubbed(:partner_request)).to be_valid
