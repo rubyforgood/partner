133726947158eaa559551d684ae7a4cc9ff87dfd
David Curtis <dacur7@gmail.com>
2018-12-02 15:55:43 -0500

    Adds and updates partners and partner requests controller specs.

--- spec/controllers/partner_requests_controller_spec.rb
+++ spec/controllers/partner_requests_controller_spec.rb
@@ -1,30 +1,56 @@
 require "rails_helper"
 
-RSpec.describe PartnerRequestsController, type: :controller do
-  login_partner
+describe PartnerRequestsController, type: :controller do
+  context "when authenticated" do
+    login_partner
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(200)
+    describe "GET #new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "POST #create" do
+      it "creates a new partner_request" do
+        expect do
+          post :create, params: { partner_request: attributes_for(:partner_request_with_items) }
+        end.to change(PartnerRequest, :count).by(1)
+      end
+    end
+
+    describe "GET #show" do
+      it "returns http success" do
+        @partner = create(:partner)
+        sign_in @partner
+        @partner_request = create(:partner_request_with_items, partner: @partner)
+        get :show, params: { id: @partner_request.id }
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
-  describe "POST #create" do
-    it "creates a new partner_request" do
-      expect do
-        post :create, params: { partner_request: attributes_for(:partner_request_with_items) }
-      end.to change(PartnerRequest, :count).by(1)
-    end
-  end
+  context "when not authenticated" do
+    describe "GET #new" do
+      subject { get :new }
 
-  describe "GET #show" do
-    it "returns http success" do
-      @partner = create(:partner)
-      sign_in @partner
-      @partner_request = create(:partner_request_with_items, partner: @partner)
-      get :show, params: { id: @partner_request.id }
-      expect(response).to have_http_status(200)
+      it_behaves_like "user is not logged in"
+    end
+
+    describe "POST #create" do
+      it "does not create a new partner_request" do
+        expect do
+          post :create, params: { partner_request: attributes_for(:partner_request_with_items) }
+        end.to_not change(PartnerRequest, :count)
+      end
+    end
+
+    describe "GET #show" do
+      subject { get :show, params: { id: partner_request.id } }
+      let(:partner) { create(:partner) }
+      let(:partner_request) { create(:partner_request_with_items, partner: partner) }
+
+      it_behaves_like "user is not logged in"
     end
   end
 end
--- spec/controllers/partners_controller_spec.rb
+++ spec/controllers/partners_controller_spec.rb
@@ -1,28 +1,69 @@
 require "rails_helper"
 
-RSpec.describe PartnersController, type: :controller do
-  login_partner
+describe PartnersController, type: :controller do
+  context "when authenticated" do
+    login_partner
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(200)
+    describe "GET #new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET #show" do
+      it "returns http success" do
+        get :show, params: { id: @partner.id }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "Get #approve" do
+      it "should redirect to partner" do
+        @partner = create(:partner)
+        get :approve, params: { partner_id: @partner.id }
+        expect(response).to redirect_to(@partner)
+      end
+    end
+
+    describe "Post #create" do
+      it "creates a new partner" do
+        expect do
+          post :create, params: { partner: attributes_for(:partner) }
+        end.to change(Partner, :count).by(0)
+      end
     end
   end
 
-  describe "Get #approve" do
-    it "should redirect to partner" do
-      @partner = create(:partner)
-      get :approve, params: { partner_id: @partner.id }
-      expect(response).to redirect_to(@partner)
-    end
-  end
+  context "when not authenticated" do
+    let(:partner) { create(:partner) }
 
-  describe "Post #create" do
-    it "creates a new partner" do
-      expect do
-        post :create, params: { partner: attributes_for(:partner) }
-      end.to change(Partner, :count).by(0)
+    describe "GET #new" do
+      subject { get :new }
+
+      it_behaves_like "user is not logged in"
+    end
+
+    describe "GET #show" do
+      subject { get :show, params: { id: partner.id } }
+
+      it_behaves_like "user is not logged in"
+    end
+
+    describe "Get #approve" do
+      subject { get :approve, params: { partner_id: partner.id } }
+
+      it_behaves_like "user is not logged in"
+    end
+
+    describe "Post #create" do
+      subject { post :create, params: { partner: attributes_for(:partner) } }
+
+      it_behaves_like "user is not logged in"
+
+      it "does not create a new partner" do
+        expect { subject }.to_not change(Partner, :count)
+      end
     end
   end
 end
