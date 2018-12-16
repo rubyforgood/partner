36d0d64f9d9aef5aed6f15cf34f82ffca49a4f92
Sean Marcia <smarcia@gmail.com>
2018-12-16 16:50:56 -0500

    Finish up devise login/logout stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery3
 //= require popper
 //= require bootstrap
 //= require main
