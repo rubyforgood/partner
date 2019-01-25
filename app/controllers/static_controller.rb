86209bd3b0745e88c8bcdeedaeb14a0c00034cb7
IlinDmitry <339966289@mail.ru>
2019-01-25 11:54:24 +0300

    Check the `:page` action for possible URL injection

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,6 +10,13 @@ class StaticController < ApplicationController
     # Anything under the url /pages/:name will render the file /app/views/static/[name].html.erb
     # Example: /pages/contact renders /app/views/static/contact.html.erb
     # Example2: /pages/index renders /app/views/static/index.html.erb, even when logged in
-    render template: "static/#{params[:name]}"
+    template = "static/#{params[:name]}"
+    if %r{[^\w\-\/]+$}.match?(template)
+      render file: "#{Rails.root}/public/400.html", status: :bad_request
+    elsif lookup_context.exists?(template)
+      render template: template
+    else
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+    end
   end
 end
---
+++ spec/controllers/static_controller_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+describe StaticController, type: :controller do
+  context "when url is correct and template is available" do
+    describe "GET #page" do
+      it "returns http success" do
+        get :page, params: { name: "index" }
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+  context "when url is correct and template isn't available" do
+    describe "GET #page" do
+      it "returns http not_found" do
+        get :page, params: { name: "index1" }
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+  context "when url characters are invalid" do
+    describe "GET #page" do
+      it "returns http bad_request" do
+        get :page, params: { name: "index[" }
+        expect(response).to have_http_status(400)
+      end
+    end
+  end
+end
