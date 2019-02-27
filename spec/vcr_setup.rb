00573bda330d558da83ffdeedfa5147cfda39628
trevordocherty <trdocher@my.loyno.edu>
2019-02-27 00:50:26 -0600

    vcr fixed style

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,13 +1,10 @@
-require 'vcr'
-
+require "vcr"
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.cassette_library_dir = "spec/vcr_cassettes"
   c.hook_into :webmock # or :fakeweb
   c.configure_rspec_metadata!
-
-
   # Let's you set default VCR mode with VCR=all for re-recording
   # episodes. :once is VCR default
   record_mode = ENV["VCR"] ? ENV["VCR"].to_sym : :once
-  c.default_cassette_options = { :record => record_mode }
+  c.default_cassette_options = { record: record_mode }
 end
