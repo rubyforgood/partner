c60b0e12ca6203b0f7eba87fba4c0ab731911340
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-03-16 23:27:17 +0000

    removed console.log sttatements

--- app/assets/javascripts/partners.js
+++ app/assets/javascripts/partners.js
@@ -1,7 +1,5 @@
 $(document).on('turbolinks:load', function() {
     $('form').on('change', '#partner_agency_type', function(event) {
-        console.log("I changed successfully");
-        console.log($(this).val() == "Other");
         if($(this).val() === "Other") {
             $('#other_agency_input').show();
         } else {
