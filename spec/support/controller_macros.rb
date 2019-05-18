243d823db63093fe9f1892f7924a66a46fdde1f3
Chase Southard <chase.southard@gmail.com>
2019-05-18 12:35:00 -0400

    adds passing options into create method

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,8 +1,8 @@
 module ControllerMacros
-  def login_partner
+  def login_partner(options={})
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:partner]
-      @partner = create(:partner)
+      @partner = create(:partner, options)
       sign_in @partner
     end
   end
