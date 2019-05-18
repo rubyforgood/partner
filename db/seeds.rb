b6e64c1185d17f4f647df22c35e155a9ceaa62a0
Chase Southard <chase.southard@gmail.com>
2019-05-18 16:19:32 -0400

    try to get seeds to parity with diaper

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,11 @@
 require 'faker'
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 puts "Creating partners..."
 puts "Adding an 'approved' partner."
 Partner.create(
     executive_director_name: "Leslie Knope",
     program_contact_name: "Leslie Knope",
-    name: "Pawnee Indiana Diaper Bank Partner",
+    name: "Pawnee Parent Service",
     address1: "123 Main St",
     address2: "",
     city: "Pawnee",
@@ -46,4 +38,68 @@ Partner.create(
     email: "unverified@example.com",
     password: "password"
 )
+
+puts "Adding a 'invited' partner."
+Partner.create(
+    name: "Pawnee Homeless Shelter",
+    email: "anyone@pawneehomelss.com",
+    partner_status: "invited",
+    diaper_bank_id: 1,
+    diaper_partner_id: 2,
+    executive_director_name: Faker::Name.name,
+    program_contact_name: Faker::Name.name,
+    name: "County Diaper Bank",
+    address1: Faker::Address.street_address,
+    address2: "",
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip_code: Faker::Address.zip,
+    website: Faker::Internet.domain_name,
+    zips_served: Faker::Address.zip,
+    executive_director_email: "anyone@pawneehomelss.com",
+    password: "password"
+)
+
+puts "Adding a 'invited' partner."
+Partner.create(
+    name: "Pawnee Pregnancy Center",
+    email: "contactus@pawneepregnancy.com",
+    partner_status: "invited",
+    diaper_bank_id: 1,
+    diaper_partner_id: 3,
+    executive_director_name: Faker::Name.name,
+    program_contact_name: Faker::Name.name,
+    name: "County Diaper Bank",
+    address1: Faker::Address.street_address,
+    address2: "",
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip_code: Faker::Address.zip,
+    website: Faker::Internet.domain_name,
+    zips_served: Faker::Address.zip,
+    executive_director_email: "contactus@pawneepregnancy.com",
+    password: "password"
+)
+
+puts "Adding a 'recertification_required' partner."
+Partner.create(
+    name: "Pawnee Senior Citizens Center",
+    email: "help@pscc.org",
+    partner_status: "recertification_required",
+    diaper_bank_id: 1,
+    diaper_partner_id: 5,
+    executive_director_name: Faker::Name.name,
+    program_contact_name: Faker::Name.name,
+    name: "County Diaper Bank",
+    address1: Faker::Address.street_address,
+    address2: "",
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip_code: Faker::Address.zip,
+    website: Faker::Internet.domain_name,
+    zips_served: Faker::Address.zip,
+    executive_director_email: "help@pscc.org",
+    password: "password"
+)
+
 puts "Done creating partners."
