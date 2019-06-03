8acf4ee5d818b38b7f8d91cdb582f4a4f02557a5
Marius Pop <marius@mlpinit.com>
2019-06-02 19:34:36 -0500

    Cleanup - Remove unused code.

--- app/models/item_request.rb
+++ app/models/item_request.rb
@@ -13,12 +13,6 @@
 #
 
 class ItemRequest < ApplicationRecord
-  RAW_JSON = '[
-    {"id":411,"partner_key":"adult_lxl","name":"Adult Briefs (Large/X-Large)"},
-    {"id":412,"partner_key":"adult_ml","name":"Adult Briefs (Medium/Large)"}
-    ]'.freeze
-  POSSIBLE_ITEMS = JSON.parse(RAW_JSON)
-
   belongs_to :partner_request, optional: true
   validates :quantity, presence: true
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
