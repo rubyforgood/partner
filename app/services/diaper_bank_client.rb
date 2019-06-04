2cefbce89b9efc022394eb55bd1838a98083b61e
Marius Pop <marius@mlpinit.com>
2019-06-03 23:46:31 -0500

    Refactor diaper bank client.

--- app/services/diaper_bank_client.rb
+++ app/services/diaper_bank_client.rb
@@ -1,24 +1,13 @@
 module DiaperBankClient
   def self.post(partner_id)
-    partner = { partner:
-      { diaper_partner_id: partner_id } }
-
-    uri = URI(ENV["DIAPERBANK_APPROVAL_URL"])
-
-    response = https(uri).request(post_request(uri: uri, body: partner.to_json))
-
-    response.body
+    diaper_post_request(
+      routes.partner_approvals,
+      { partner: { diaper_partner_id: partner_id } }.to_json
+    ).body
   end
 
   def self.get_available_items(diaper_bank_id)
-    uri = URI(ENV["DIAPERBANK_PARTNER_REQUEST_URL"] + "/#{diaper_bank_id}")
-    req = Net::HTTP::Get.new(uri)
-
-    req["Content-Type"] = "application/json"
-    req["X-Api-Key"] = ENV["DIAPERBANK_KEY"]
-
-    response = https(uri).request(req)
-
+    response = diaper_get_request(routes.partner_requests(diaper_bank_id))
     if response.code.to_i == 200
       JSON.parse(response.body)
     else
@@ -28,23 +17,19 @@ module DiaperBankClient
 
   def self.send_family_request(family_request_id)
     return unless family_request = FamilyRequest.find(family_request_id)
-
-    uri = URI(ENV["DIAPERBANK_FAMILY_REQUEST_URL"])
-    body = family_request.export_json
-    response = https(uri).request(post_request(uri: uri, body: body))
-
+    response = diaper_post_request(
+      routes.family_requests,
+      family_request.export_json
+    )
     response.body ? JSON.parse(response.body) : nil
   end
 
   def self.request_submission_post(partner_request_id)
     return unless PartnerRequest.exists?(partner_request_id)
-
-    uri = URI(ENV["DIAPERBANK_PARTNER_REQUEST_URL"])
-    body = PartnerRequest.find(partner_request_id).export_json
-
-    response = https(uri).request(post_request(uri: uri, body: body))
-
-    response.body
+    diaper_post_request(
+      routes.partner_requests,
+      PartnerRequest.find(partner_request_id).export_json
+    ).body
   end
 
   def self.https(uri)
@@ -55,11 +40,40 @@ module DiaperBankClient
     end
   end
 
+  def self.diaper_post_request(uri, body)
+    https(uri).request(post_request(uri: uri, body: body))
+  end
+
+  def self.diaper_get_request(uri)
+    request = Net::HTTP::Get.new(uri)
+    request["Content-Type"] = "application/json"
+    request["X-Api-Key"] = ENV["DIAPERBANK_KEY"]
+    https(uri).request(request)
+  end
+
   def self.post_request(uri:, body:, content_type: "application/json")
-    req = Net::HTTP::Post.new(uri)
-    req.body = body
-    req["Content-Type"] = content_type
-    req["X-Api-Key"] = ENV["DIAPERBANK_KEY"]
-    req
+    request = Net::HTTP::Post.new(uri)
+    request.body = body
+    request["Content-Type"] = content_type
+    request["X-Api-Key"] = ENV["DIAPERBANK_KEY"]
+    request
+  end
+
+  def self.routes
+    Routes.instance
+  end
+
+  class Routes
+    include Singleton
+
+    def initialize
+      @endpoint = ENV["DIAPERBANK_ENDPOINT"]
+    end
+
+    [:partner_requests, :family_requests, :partner_approvals].each do |path|
+      define_method(path) do |id = ''|
+        URI("#{@endpoint}/#{path}/#{id}")
+      end
+    end
   end
 end
