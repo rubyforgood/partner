bdff855ebd481d8b5b319e7db41a4601821cf062
Marius Pop <marius@mlpinit.com>
2019-06-09 19:55:30 -0500

    Make build green

--- spec/features/child_feature_spec.rb
+++ spec/features/child_feature_spec.rb
@@ -10,7 +10,7 @@ describe Child, type: :feature, js: true do
 
   scenario "User can see a list of children" do
     diaper_type = "Magic diaper"
-    stub_request(:any, "#{ENV["DIAPERBANK_PARTNER_REQUEST_URL"]}/#{partner.id}")
+    stub_request(:any, "#{ENV["DIAPERBANK_ENDPOINT"]}/partner_requests/#{partner.id}")
       .to_return(body: [{ id: 1, name: diaper_type }].to_json, status: 200)
 
     family = create(:family, partner: partner)
@@ -28,24 +28,9 @@ describe Child, type: :feature, js: true do
         expect(find("tr:nth-child(#{index + 1}) td:nth-child(3)"))
           .to have_text(child.date_of_birth)
         expect(find("tr:nth-child(#{index + 1}) td:nth-child(4)"))
-          .to have_text(child.gender)
+          .to have_text(child.family.guardian_display_name)
         expect(find("tr:nth-child(#{index + 1}) td:nth-child(5)"))
-          .to have_text(child.child_lives_with)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(6)"))
-          .to have_text(child.race)
-
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(8)"))
-          .to have_text(child.health_insurance)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(9)"))
-          .to have_text(diaper_type)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(10)"))
           .to have_text(child.comments)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(11)"))
-          .to have_text("Show")
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(12)"))
-          .to have_text("Edit")
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(13)"))
-          .to have_text("Destroy")
       end
     end
   end
