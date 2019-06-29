e4931def87b88736ba2ff31b272da44df3703fe2
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-06-30 03:02:27 +0800

    adds military family checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_003648) do
+ActiveRecord::Schema.define(version: 2019_06_29_182738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_003648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "partner_id"
+    t.boolean "military", default: false
     t.index ["partner_id"], name: "index_families_on_partner_id"
   end
 
@@ -214,7 +215,6 @@ ActiveRecord::Schema.define(version: 2019_05_28_003648) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
-    t.string "other_agency_type"
     t.index ["diaper_bank_id"], name: "index_partners_on_diaper_bank_id"
     t.index ["email"], name: "index_partners_on_email", unique: true
     t.index ["invitation_token"], name: "index_partners_on_invitation_token", unique: true
