a1aef6e8418128ed18908aad7d76d6a4a41aad7d
Benjamin Reynolds <breyno127@gmail.com>
2019-07-21 15:58:44 -0500

    Regenerate model annotations

--- app/models/authorized_family_member.rb
+++ app/models/authorized_family_member.rb
@@ -2,15 +2,16 @@
 #
 # Table name: authorized_family_members
 #
-#  id                   :bigint(8)        not null, primary key
-#  first_name           :string
-#  last_name            :string
-#  date_of_birth        :date
-#  gender               :string
-#  comments             :text
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  family_id            :bigint(8)
+#  id            :bigint(8)        not null, primary key
+#  first_name    :string
+#  last_name     :string
+#  date_of_birth :date
+#  gender        :string
+#  comments      :text
+#  family_id     :bigint(8)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 
 class AuthorizedFamilyMember < ApplicationRecord
   belongs_to :family
