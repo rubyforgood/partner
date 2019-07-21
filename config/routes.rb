3abbf376820b75503dcb3fa465a6e6d905974a0a
Benjamin Reynolds <breyno127@gmail.com>
2019-07-21 16:35:45 -0500

    Generate and migrate user as a devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # TODO: remove these two
   resources :children do
     post :active
---
+++ spec/factories/users.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
+FactoryBot.define do
+  factory :user do
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+  end
+end
