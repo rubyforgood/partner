b136e0781b8b19060288599823d6ba369f76849f
Benjamin Reynolds <breyno127@gmail.com>
2019-07-26 13:38:55 -0400

    Remove devise columns from the partners table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_153432) do
+ActiveRecord::Schema.define(version: 2019_07_26_172501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -210,32 +210,8 @@ ActiveRecord::Schema.define(version: 2019_07_26_153432) do
     t.string "diaper_funding_source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer "invitation_limit"
-    t.string "invited_by_type"
-    t.bigint "invited_by_id"
-    t.integer "invitations_count", default: 0
     t.string "other_agency_type"
     t.index ["diaper_bank_id"], name: "index_partners_on_diaper_bank_id"
-    t.index ["email"], name: "index_partners_on_email", unique: true
-    t.index ["invitation_token"], name: "index_partners_on_invitation_token", unique: true
-    t.index ["invitations_count"], name: "index_partners_on_invitations_count"
-    t.index ["invited_by_id"], name: "index_partners_on_invited_by_id"
-    t.index ["invited_by_type", "invited_by_id"], name: "index_partners_on_invited_by_type_and_invited_by_id"
-    t.index ["reset_password_token"], name: "index_partners_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
