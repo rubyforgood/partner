1811c4ed1bafffcdefd3da88edaad86fa4c837b5
Gregory Dombchik <gbdjobs1@gmail.com>
2019-07-26 15:59:02 -0400

    fix rubocop violations

--- spec/features/authorized_family_member_feature_spec.rb
+++ spec/features/authorized_family_member_feature_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe AuthorizedFamilyMember, type: :feature do
   let!(:partner) { create(:partner, id: 3) }
-  let!(:family) { create(:family, partner: partner)}
+  let!(:family) { create(:family, partner: partner) }
 
   before do
     sign_in(partner)
@@ -16,8 +16,8 @@ describe AuthorizedFamilyMember, type: :feature do
     expect(page).to have_current_path(family_path(family))
     click_link("Add An Authorized Member To This Family")
     expect(page).to have_current_path(new_authorized_family_member_path, ignore_query: true)
-    fill_in('First name', with: 'John')
-    fill_in('Last name', with: 'Smith')
+    fill_in("First name", with: "John")
+    fill_in("Last name", with: "Smith")
     click_button
     expect(AuthorizedFamilyMember.count).to eq 1
   end
