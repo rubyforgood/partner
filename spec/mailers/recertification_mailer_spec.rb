5e09e1e32c10b8c30615b5b96bc0aacb9dc45c45
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-07-27 15:25:50 -0400

    creates tests for email subject and body

---
+++ spec/mailers/recertification_mailer_spec.rb
@@ -0,0 +1,24 @@
+describe RecertificationMailer, type: :mailer do
+  describe 'notice_email' do
+    let!(:partner) { create(:partner) }
+    let!(:mail) { RecertificationMailer.with(partner: partner).notice_email.deliver_now }
+
+    it 'email renders the subject' do
+      expect(mail.subject).to eq('Please recertify your account')
+    end
+
+    it 'email renders the receiver email' do
+      expect(mail.to).to eq([partner.email])
+    end
+
+    it 'email renders the sender email' do
+      expect(mail.from).to eq(['partner@diaper-app.org'])
+    end
+
+    it 'email assigns @recertification_url' do
+      expect(mail.body.encoded)
+      .to match(edit_partner_url(partner))
+    end
+  end
+end
+
