67d4df48f908d3016acc26e1353542f5a46b4582
Chase Southard <chase.southard@gmail.com>
2019-07-27 16:02:24 -0400

    Add simplecov (#146)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "simplecov"
+SimpleCov.start "rails"
+
 require "spec_helper"
 require "shoulda/matchers"
 ENV["RAILS_ENV"] ||= "test"
