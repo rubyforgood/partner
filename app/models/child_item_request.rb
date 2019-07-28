1eb570389147e759ff1d303b461c8372776aab9d
Marius Pop <marius@mlpinit.com>
2019-07-28 10:43:55 -0400

    Add pickup sheet.

--- app/models/child_item_request.rb
+++ app/models/child_item_request.rb
@@ -1,4 +1,8 @@
 class ChildItemRequest < ApplicationRecord
   belongs_to :item_request
   belongs_to :child
+
+  def quantity
+    item_request.quantity.to_i / item_request.children.size
+  end
 end
