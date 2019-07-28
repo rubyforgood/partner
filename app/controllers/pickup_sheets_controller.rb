81c44ec24303531793be4fe53120bd01eead2c9f
Marius Pop <marius@mlpinit.com>
2019-07-28 10:43:55 -0400

    Add pickup sheet.

--- app/controllers/pickup_sheets_controller.rb
+++ app/controllers/pickup_sheets_controller.rb
@@ -1,14 +1,21 @@
 class PickupSheetsController < ApplicationController
   before_action :authenticate_partner!
 
-  helper_method :family_request
+  helper_method :partner_request, :child_item_requests
 
-  def show;end
+  def show; end
 
   private
 
-  def family_request
-    @family_request ||= current_partner.family_requests
-      .find(params[:family_request_id])
+  def partner_request
+    @partner_request ||= current_partner.partner_requests.includes(item_requests: :children).find(params[:family_request_id])
+  end
+
+  def child_item_requests
+    @child_item_requests ||= partner_request.item_requests.map do |item_request|
+      item_request.child_item_requests
+    end.flatten.sort_by do |child_item_request|
+      child_item_request.child.display_name
+    end
   end
 end
