999debdf50dfeeadf1ee65722304e1c302e250d1
Marius Pop <marius@mlpinit.com>
2019-07-28 11:09:25 -0400

    Add annotation.

--- app/models/child_item_request.rb
+++ app/models/child_item_request.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: child_item_requests
+#
+#  id              :bigint(8)        not null, primary key
+#  child_id        :bigint(8)
+#  item_request_id :bigint(8)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class ChildItemRequest < ApplicationRecord
   belongs_to :item_request
   belongs_to :child
