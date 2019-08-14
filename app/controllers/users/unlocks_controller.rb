068cc4d41f8d8a94026b31b69643f8100ba4cb6c
Benjamin Reynolds <breyno127@gmail.com>
2019-08-14 15:08:19 -0500

    Rename partners devise controllers to users

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Partners::UnlocksController < Devise::UnlocksController
+class Users::UnlocksController < Devise::UnlocksController
   # GET /resource/unlock/new
   # def new
   #   super
