56645d205951a57e18a2cb8349de8ff57fcc59f5
Cassiano Blonski Sampaio <cassiano.sampaio@codeminer42.com>
2019-08-26 17:00:47 -0300

    Refactor child feature spec

--- spec/features/child_feature_spec.rb
+++ spec/features/child_feature_spec.rb
@@ -1,38 +1,51 @@
 require "rails_helper"
 
 describe Child, type: :feature, js: true do
-  let(:partner) { create(:partner, id: 3) }
-  let(:user) { create(:user, partner: partner) }
+  let!(:partner) { create(:partner, id: 3) }
+  let!(:user)    { create(:user, partner: partner) }
+  let!(:family)  { create(:family, partner: partner) }
 
   before do
     sign_in(user)
-    visit(root_path)
+
+    stub_request(:any, "#{ENV["DIAPERBANK_ENDPOINT"]}/partner_requests/#{partner.id}")
+      .to_return(body: [{ id: 1, name: "Magic diaper" }].to_json, status: 200)
   end
 
-  scenario "User can see a list of children" do
-    diaper_type = "Magic diaper"
-    stub_request(:any, "#{ENV["DIAPERBANK_ENDPOINT"]}/partner_requests/#{partner.id}")
-      .to_return(body: [{ id: 1, name: diaper_type }].to_json, status: 200)
+  describe "Show" do
+    let!(:children) { create_list(:child, 2, family: family) }
 
-    family = create(:family, partner: partner)
-    children = [
-      create(:child, family: family),
-      create(:child, family: family)
-    ].reverse
-    click_link "Children"
-    children.each.with_index do |child, index|
-      within "tbody" do
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(1)"))
-          .to have_text(child.first_name)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(2)"))
-          .to have_text(child.last_name)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(3)"))
-          .to have_text(child.date_of_birth)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(5)"))
-          .to have_text(child.family.guardian_display_name)
-        expect(find("tr:nth-child(#{index + 1}) td:nth-child(6)"))
-          .to have_text(child.comments)
+    before do
+      visit(root_path)
+    end
+
+    it "displays a list of children" do
+      click_link "Children"
+      children.each do |child|
+        within "tbody" do
+          expect(page).to have_text(child.first_name) &
+                          have_text(child.last_name) &
+                          have_text(child.date_of_birth) &
+                          have_text(child.family.guardian_display_name) &
+                          have_text(child.comments)
+        end
       end
+    end
+  end
+
+  describe "New" do
+    before do
+      sign_in(user)
+      visit(family_path(family))
+    end
+
+    it "creates a child to a family member" do
+      click_link("Add Child To This Family", match: :first)
+      fill_in("First name", with: "John")
+      fill_in("Last name", with: "Smith")
+      click_button
+
+      expect(Child.count).to eq 1
     end
   end
 end
