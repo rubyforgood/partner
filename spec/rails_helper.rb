3fc4114e9eb91080b0e7b74df75c53d3703995f2
Cassiano Blonski Sampaio <cassiano.sampaio@codeminer42.com>
2019-08-29 16:27:34 -0300

    Migrate partner requests controller spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ require "devise"
 require "capybara/rails"
 require "capybara/rspec"
 require "capybara-screenshot/rspec"
-require_relative "support/controller_macros"
+
 # Add additional requires below this line. Rails is not loaded until this point!
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -137,5 +137,6 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::ControllerHelpers,  type: :controller
   config.extend ControllerMacros, type: :controller
+  config.include RequestSpecHelper, type: :request
   config.include FactoryBot::Syntax::Methods
 end
