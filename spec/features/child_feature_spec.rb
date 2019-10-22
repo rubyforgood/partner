d0b659f05646e3a8915bb000267b97de7fe04d63
Marius Pop <marius@mlpinit.com>
2019-10-22 18:53:14 -0400

    Add spec for child show view

--- spec/features/child_feature_spec.rb
+++ spec/features/child_feature_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
-describe Child, type: :feature do
-  let(:partner) { create(:partner) }
+describe Child, type: :feature, include_shared: true, js: true do
+  let(:partner) { create(:partner, :verified, id: 3) }
   let(:user) { create(:user, partner: partner) }
+  let(:family) { create(:family, partner: partner) }
 
   before do
     Flipper[:family_requests].enable(partner)
@@ -15,7 +16,6 @@ describe Child, type: :feature do
     stub_request(:any, "#{ENV["DIAPERBANK_ENDPOINT"]}/partner_requests/#{partner.id}")
       .to_return(body: [{ id: 1, name: diaper_type }].to_json, status: 200)
 
-    family = create(:family, partner: partner)
     children = [
       create(:child, last_name: "Zeno", family: family),
       create(:child, last_name: "Arthur", family: family)
@@ -34,6 +34,29 @@ describe Child, type: :feature do
           .to have_text(child.family.guardian_display_name)
         expect(find("tr:nth-child(#{index + 1}) td:nth-child(6)"))
           .to have_text(child.comments)
+      end
+    end
+  end
+
+  describe "Show View" do
+    let!(:child) { create(:child, last_name: "Zeno", family: family) }
+
+    scenario "User can see a list of requested items" do
+      stub_successful_items_partner_request
+      stub_successful_family_request
+      visit partner_requests_path
+      find_link("Create New Family Diaper Request").click
+      find('input[type="submit"]').click
+      expect(find("h3")).to have_text("Diaper Request History")
+      visit child_path(child)
+      within "tbody" do
+        within find("tr:nth-child(1)") do
+          expect(find("td:nth-child(1)")).to have_text(Date.today.iso8601)
+          expect(find("td:nth-child(2)")).to have_text('Fantastic diaper')
+          expect(find("td:nth-child(3)")).to have_text('Not picked up')
+          expect(find("td:nth-child(4)")).to have_text('100')
+          expect(find("td:nth-child(5)")).to have_text('Not picked up')
+        end
       end
     end
   end
