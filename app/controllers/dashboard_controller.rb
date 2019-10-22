e37dfa4dd2dd95145175ec09a456ccd8e4a9a2a4
Meg Gutshall <meghan.gutshall@gmail.com>
2019-10-22 13:57:58 -0400

    Run rubocop

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -24,5 +24,4 @@ class DashboardController < ApplicationController
   #   @recent_donations_from_manufacturers = current_organization.donations.includes(:line_items).during(helpers.selected_range).by_source(:manufacturer)
   #   @top_manufacturers = current_organization.manufacturers.by_donation_count
   # end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   )
 
   get "dashboard", to: "dashboard#index"
-  
+
   resources :partners do
     get :approve
   end
