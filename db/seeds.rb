e4882b50d58b86c68d59500fec1f873c10277a56
Meg Gutshall <meghan.gutshall@gmail.com>
2019-10-23 22:41:10 -0400

    Adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ User.create(
     partner: verified_partner
 )
 puts "Adding families."
-5.times do
+Faker::Number.within(range: 4..13).times do
     Family.create(
         guardian_first_name: Faker::Name.first_name,
         guardian_last_name: Faker::Name.last_name,
@@ -52,36 +52,36 @@ puts "Adding children."
 Family.all.each do |family|
     family.home_child_count.times do
         Child.create(
+            family: family,
             first_name: Faker::Name.first_name,
-            last_name: Faker::Name.last_name,
+            last_name: family.guardian_last_name,
             date_of_birth: Faker::Date.birthday(min_age: 5, max_age: 18),
             gender: Faker::Gender.binary_type,
             child_lives_with: Child::CAN_LIVE_WITH.sample(2),
             race: Child::RACES.sample,
-            agency_child_id: Faker::Name.name,
+            agency_child_id: family.agency_guardian_id,
             health_insurance: family.guardian_health_insurance,
             comments: Faker::Lorem.paragraph,
             active: Faker::Boolean.boolean,
             archived: false,
-            item_needed_diaperid: Child::CHILD_ITEMS.sample,
-            family: family
+            item_needed_diaperid: Child::CHILD_ITEMS.sample
         )
     end
     family.home_young_child_count.times do
         Child.create(
+            family: family,
             first_name: Faker::Name.first_name,
-            last_name: Faker::Name.last_name,
+            last_name: family.guardian_last_name,
             date_of_birth: Faker::Date.birthday(min_age: 0, max_age: 5),
             gender: Faker::Gender.binary_type,
             child_lives_with: Child::CAN_LIVE_WITH.sample(2),
             race: Child::RACES.sample,
-            agency_child_id: Faker::Name.name,
+            agency_child_id: family.agency_guardian_id,
             health_insurance: family.guardian_health_insurance,
             comments: Faker::Lorem.paragraph,
             active: Faker::Boolean.boolean,
             archived: false,
-            item_needed_diaperid: Child::CHILD_ITEMS.sample,
-            family: family
+            item_needed_diaperid: Child::CHILD_ITEMS.sample
         )
     end
 end
@@ -99,7 +99,7 @@ Family.all.each do |family|
 end
 
 puts "Adding diaper requests."
-40.times do
+Faker::Number.within(range: 32..56).times do
     PartnerRequest.create(
         comments: Faker::Lorem.paragraph,
         partner: verified_partner,
