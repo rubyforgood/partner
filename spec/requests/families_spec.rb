0c6550bebbf31b70e904d4e30a8831b9e0edd647
Megan O'Neill <oneill38@github.com>
2019-10-24 19:49:41 -0700

    increase test coverage for the families controller

--- spec/requests/families_spec.rb
+++ spec/requests/families_spec.rb
@@ -24,6 +24,42 @@ RSpec.describe "Families", type: :request do
     end
   end
 
+  describe "GET #show" do
+    let(:family) { create(:family, partner: partner) }
+
+    it "should return status code 200 if a family is found" do
+      get family_path(family.id)
+
+      expect(response).to have_http_status :ok
+    end
+
+    it "should render 404 for a family not found" do
+      max_id = family.id + 1
+
+      get family_path(max_id)
+
+      expect(response).to have_http_status :not_found
+    end
+  end
+
+  describe "GET #edit" do
+    let(:family) { create(:family, partner: partner) }
+
+    it "should render the edit template" do
+      get edit_family_path(family.id)
+
+      expect(response).to render_template(:edit)
+    end
+
+    it "should render 404 for a family not found" do
+      max_id = family.id + 1
+
+      get edit_family_path(max_id)
+
+      expect(response).to have_http_status :not_found
+    end
+  end
+
   describe "POST #create" do
     it "should create and redirect to family_path" do
       post families_path, params: { family: attributes_for(:family) }
@@ -32,6 +68,15 @@ RSpec.describe "Families", type: :request do
 
       expect(response).to redirect_to(family_path(family.id))
       expect(request.flash[:notice]).to eql "Family was successfully created."
+    end
+
+    it "renders :new if the create action does not succeed" do
+      post families_path, params: { family:  { guardian_first_name: nil } }
+
+      family = Family.last
+
+      expect(response).to render_template(:new)
+      expect(family).to be_nil
     end
   end
 
@@ -43,6 +88,14 @@ RSpec.describe "Families", type: :request do
 
       expect(response).to redirect_to(family_path(family.id))
       expect(request.flash[:notice]).to eql "Family was successfully updated."
+    end
+
+    it "renders :edit if the update does not succeed" do
+      put family_path(family), params: { family: { guardian_first_name: nil } }
+
+      expect(response).to have_http_status :ok
+      expect(response).to render_template(:edit)
+      expect(family.guardian_first_name).not_to be_nil
     end
   end
 
