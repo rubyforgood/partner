cd96d75033bacaa99bbcac6f33390aeded9a1afb
Gabriel Baldão <gabriel_cbaldao@hotmail.com>
2019-11-25 13:43:32 -0300

    add button to export children as csv (#266)
    * add button to export children as csv
    
    * Update children_controller_spec.rb
    
    * Update child.rb

---
+++ spec/controllers/children_controller_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe ChildrenController, type: :controller do
+  let(:family)  { create :family }
+  let(:partner) { create :partner, name: "new partner", family_ids: [family.id] }
+  let!(:child) { create :child, family: family }
+  let(:user) { create :user, partner: partner }
+
+  before do
+    sign_in user
+  end
+  describe "GET #index" do
+    context "when responding csv" do
+      subject { get :index, format: :csv }
+
+      it "responds correct Content-Type" do
+        expect(subject.content_type).to eq "text/csv"
+      end
+    end
+  end
+end
