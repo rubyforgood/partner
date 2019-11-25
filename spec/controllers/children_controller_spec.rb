350ff33bca17bd5dbee8963aea485d39876b61bb
Gabriel Baldão <gabrielbaldao@gmail.com>
2019-11-25 11:59:35 -0300

    Update children_controller_spec.rb

--- spec/controllers/children_controller_spec.rb
+++ spec/controllers/children_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe ChildrenController, type: :controller do
-  let(:family)  { create :family}
+  let(:family)  { create :family }
   let(:partner) { create :partner, name: "new partner", family_ids: [family.id] }
   let!(:child) { create :child, family: family }
   let(:user) { create :user, partner: partner }
@@ -10,7 +10,7 @@ describe ChildrenController, type: :controller do
     sign_in user
   end
   describe "GET #index" do
-    describe "when responding csv" do
+    context "when responding csv" do
       subject { get :index, format: :csv }
 
       it "responds correct Content-Type" do
