f065400f6e3bee8aefa24c551e3b35e0ac548e5b
Sean Marcia <smarcia@gmail.com>
2020-06-13 12:13:14 -0400

    Put new theme into partnerbase. (#328)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "partner.test"
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
