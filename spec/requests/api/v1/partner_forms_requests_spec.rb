b00ec3b7a3e6a437156b5ee64eeb4498f3229715
Robyn-Dale Samuda <robyn@yuraki.com>
2020-06-19 14:46:16 -0500

    Add test for invalid API key to partner forms endpoint

--- spec/requests/api/v1/partner_forms_requests_spec.rb
+++ spec/requests/api/v1/partner_forms_requests_spec.rb
@@ -2,26 +2,43 @@ require "rails_helper"
 
 describe "Partners API Requests", type: :request do
   describe "POST /api/v1/partners" do
-    context "when the body is empty" do
-      it "responds with an error status code" do
-        post api_v1_partners_path({}), headers: { 'X-Api-Key': ENV["DIAPER_KEY"] }
+    context "with valid API key" do
+      context "when the body is empty" do
+        it "responds with an error status code" do
+          post api_v1_partners_path({}), headers: { 'X-Api-Key': ENV["DIAPER_KEY"] }
 
-        expect(response).to have_http_status(:bad_request)
+          expect(response).to have_http_status(:bad_request)
+        end
+      end
+
+      context "when given valid parameters" do
+        it "responds with an :ok status" do
+          valid_partner_form_creation_request
+
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "creates a new `PartnerForm` record" do
+          expect { valid_partner_form_creation_request }
+            .to change { PartnerForm.count }
+            .from(0)
+            .to(1)
+        end
       end
     end
 
-    context "when given valid parameters" do
-      it "responds with an :ok status" do
-        valid_partner_form_creation_request
+    context "with invalid API key" do
+      it "responds with forbidden" do
+        expect do
+          post api_v1_partner_forms_path(
+            partner_form: {
+              diaper_bank_id: 1,
+              sections: %w(section1 section2 section3)
+            }
+          ), headers: { 'X-Api-Key': "INVALID" }
+        end.to_not change { PartnerForm.count }
 
-        expect(response).to have_http_status(:ok)
-      end
-
-      it "creates a new `PartnerForm` record" do
-        expect { valid_partner_form_creation_request }
-          .to change { PartnerForm.count }
-          .from(0)
-          .to(1)
+        expect(response).to have_http_status(:forbidden)
       end
     end
   end
