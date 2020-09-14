d58631fcdd14b38f730f476be013bc8337e0ee98
fosterv2 <fosterv2@outlook.com>
2020-09-14 14:31:47 -0700

    Fix rubocop offense

--- spec/models/impact_story_spec.rb
+++ spec/models/impact_story_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe ImpactStory, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:content) }
   end
-  
+
   describe '#blurb' do
     subject { impact_story.blurb(10) }
     let(:impact_story) { create(:impact_story, title: Faker::Lorem.word, content: Faker::Lorem.paragraph) }
-      
+
     it "returns truncates the content to less than or equal to 10 characters" do
       expect(subject.length).to be <= 20
     end
