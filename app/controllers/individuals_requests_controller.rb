b428e4cc8ce6c46baec2fe70f67e15e7a42fb882
Carlos Palhares <chjunior@gmail.com>
2020-09-24 00:27:04 -0300

    Use FamilyRequest model on individuals requests

--- app/controllers/individuals_requests_controller.rb
+++ app/controllers/individuals_requests_controller.rb
@@ -5,5 +5,15 @@ class IndividualsRequestsController < ApplicationController
   before_action :verify_status_in_diaper_base
   before_action :authorize_verified_partners
 
-  def new; end
+  def new
+    @request = FamilyRequest.new({}, initial_items: 1)
+  end
+
+  def create
+    @request = FamilyRequest.new(
+      params.require(:family_request)
+            .permit(:comments, items_attributes: %i[item_id people_count])
+    )
+    render :new
+  end
 end
