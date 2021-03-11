a541722a16f9a4e489a8f34ae198374d39e07f65
Edwin Mak <edwinthinks@gmail.com>
2021-03-11 07:12:33 -0500

    Heroku ready (#558)
    * Add Procfile
    
    * Update database.yml
    
    * Configure emailing to be off using ENV var
    
    * Update config/database.yml
    
    * Remove rspec-push since heroku will auto deploy this;

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name 			=> ENV['SENDGRID_USERNAME'],
-  :password 			=> ENV['SENDGRID_PASSWORD'],
-  :domain         => 'diaper.app',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'diaper.app',
   :enable_starttls_auto => true
 }
